dd4bc5e91f36590a0380c07da6ab25994e599589
Duvan Munoz <63968663+duvan051@users.noreply.github.com>
2026-03-29 15:36:26 -0500

    feat: integrate PostgreSQL with Prisma using native adapter and dotenv for environment variables
    - Added PostgreSQL adapter for Prisma in package.json
    - Updated Prisma schema for compatibility
    - Configured dotenv to manage environment variables
    - Enhanced PrismaService to utilize PostgreSQL connection pooling

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,30 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg"; // Necesitamos el Pool de la librería 'pg'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    // 1. Creamos el pool de conexiones nativo de Postgres
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+    // 2. Creamos el adaptador de Prisma usando ese pool
+    const adapter = new PrismaPg(pool);
+
+    // 3. Pasamos el adaptador al constructor del cliente
+    super({ adapter });
+
+    console.log('🚀 PrismaService inicializado con Native Adapter (pg)');
+  }
+
   async onModuleInit() {
+    // Con el adaptador, esto abrirá la conexión a través del pool de 'pg'
     await this.$connect();
+    console.log('✅ Conexión exitosa mediante Adaptador');
   }
 
   async onModuleDestroy() {
